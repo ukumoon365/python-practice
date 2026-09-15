feaf832f436fdb302364168dbb74bd6f1de3dc0b
Mun kyeonguk <ukumoon365@gmail.com>
2026-09-15 19:42:11 +0900

    Solve: Q8 툭정 이름의 위치

---
+++ data-structure/easy/8.py
@@ -0,0 +1,19 @@
+# 특정 이름의 위치
+"""
+1. 이름을 입력 
+2. 리스트에 명단이 있는지 확인
+   있으면 -> n번대에 있습니다
+   없으면 -> 명단에 없습니다
+"""
+
+# 명단
+names = ["윤서", "지우", "민준", "서윤", "도윤", "예준"]
+
+# 이름 입력
+name = input()
+
+if name in names :
+    name_number = names.index(name) + 1     # 입력 받은 이름을 .index를 통해 추출, index는 0부터 시작하기에 + 1  
+    print(f"{name_number}번째에 있습니다")
+else:
+    print("명단에 없습니다")
